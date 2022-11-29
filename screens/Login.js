f0ed6a109f6d3b4e3914567fd9e3cbff58fc1e8e
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-11-29 10:49:36 -0300

    Processo de login

--- screens/Login.js
+++ screens/Login.js
@@ -1,49 +1,89 @@
-import { Button, StyleSheet, TextInput, View } from 'react-native'
+import { useState } from "react";
+import {
+  Alert,
+  Pressable,
+  StyleSheet,
+  TextInput,
+  Text,
+  View,
+} from "react-native";
+/* Importamos os recursos de autentificação através das configurações firebase */
+import { auth } from "../firebaseConfig.js";
+/* Importamos as funções de autentificação diretamente da lib */
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
 
-    return (
-        <View style={estilos.container}>
-            <View style={estilos.formulario}>
-                <TextInput
-                    placeholder='E-mail'
-                    style={estilos.input}
-                />
-                <TextInput
-                    placeholder='Senha'
-                    style={estilos.input}
-                />
-                <View style={estilos.botoes}>
-                    <Button title='Entre' color="green" />
-                </View>
-            </View>
+  const login = () => {
+    if (!email || !senha) {
+      Alert.alert("Atenção", "Você deve preencher todos os campos");
+      return;
+    }
+
+    signInWithEmailAndPassword(auth, email, senha)
+      .then(() => {
+        console.log("Usuário foi logado!");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <View style={estilos.container}>
+      <View style={estilos.formulario}>
+        <TextInput
+          placeholder="E-mail"
+          style={estilos.input}
+          keyboardType="email-address"
+          onChangeText={(valor) => setEmail(valor)}
+        />
+        <TextInput
+          placeholder="Senha"
+          style={estilos.input}
+          secureTextEntry
+          onChangeText={(valor) => setSenha(valor)}
+        />
+        <View>
+          <Pressable style={estilos.botoes} onPress={login}>
+            <Text style={estilos.titulo}>Entre</Text>
+          </Pressable>
         </View>
-    )
-}
+      </View>
+    </View>
+  );
+};
 
-export default Login
+export default Login;
 
 const estilos = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'lightgreen',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    formulario: {
-        marginBottom: 32,
-        width: "80%"
-    },
-    input: {
-        backgroundColor: "white",
-        marginVertical: 8,
-        padding: 8,
-        borderRadius: 4
-    },
-    botoes: {
-        marginVertical: 8,
-        width: "100%",
-        flexDirection: "row",
-        justifyContent: "space-between"
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "lightgreen",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  formulario: {
+    marginBottom: 32,
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    marginVertical: 8,
+    padding: 8,
+    borderRadius: 4,
+  },
+  botoes: {
+    backgroundColor: "green",
+    marginVertical: 8,
+    padding: 10,
+    width: "100%",
+    borderRadius: 4,
+  },
+  titulo: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 16,
+  },
+});
